74e2cf10394ce44d2706c329bb9831168ba3be93
Piotr_Berezka <berezkapiotr@gmail.com>
2022-02-15 20:55:13 +0100

    initial setup

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LandingPage: React.FC = () => {
+  return (
+    <React.Fragment>
+      <h1>
+        GUWNO
+      </h1>
+    </React.Fragment>
+  );
+}
+
+export default LandingPage;
